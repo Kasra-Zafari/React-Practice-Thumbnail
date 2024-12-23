b281aeb64e536db699fc6b9935baa4f9d15288d3
Kasra Zafari <160012548+Kasra-Zafari@users.noreply.github.com>
2024-12-23 19:31:09 +0330

    finish

--- Thumbnail/src/componets/Thumbnail/index.jsx
+++ Thumbnail/src/componets/Thumbnail/index.jsx
@@ -3,22 +3,28 @@ import { useState } from "react"
 import classes from "./index.module.css"
 
 const Thumbnail = () => {
-    const [cover, setCover] = useState(IMAGE[0].image)
+    const [cover, setCover] = useState(IMAGE[0].image);
+    const [selectedImg, setSelectedImg] = useState(0);
 
-    const handleCover = (item) => {
-        setCover(item)
+    const handleCover = (item, index) => {
+        setCover(item);
+        setSelectedImg(index)
     }
 
     return (
         <>
-            <div className={classes["thumbnail-container"]}>
+            <div className={classes.thumbnailContainer}>
             <div className={classes.cover}>
                 <img src={cover} alt="" />
             </div>
 
-            <div className={classes["thumbnail-list"]}>
+            <div className={classes.thumbnailList}>
                 {IMAGE.map((imageData, index) => {
-                    return <img onClick={() => handleCover(imageData.image)} key={index} src={imageData.image} alt="" />
+                    return <img
+                    onClick={() => handleCover(imageData.image, index)}
+                    key={index}
+                    src={imageData.image}
+                    className={index === selectedImg && classes.selected} alt="" />
                 })}
             </div>
             </div>
